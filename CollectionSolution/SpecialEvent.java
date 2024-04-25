b7f4ee73349b50263e343318253db34e7b2e4ae5
Mr. Park <29616719+mrparkonline@users.noreply.github.com>
2024-04-25 09:20:34 -0400

    Add files via upload

---
+++ CollectionSolution/SpecialEvent.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class SpecialEvent {
+    public static void main(String[] args) {
+        // Special Events
+        Scanner sc = new Scanner(System.in);
+        // Setup:
+        System.out.println("Input the Number of people invited.");
+        int people = Integer.parseInt(sc.nextLine());
+        int[] days = {0,0,0,0,0}; // For availability
+        for (int i = 0; i < people; i++) {
+            System.out.println("Input the Person's availability:");
+            String schedule = sc.nextLine(); 
+            // Would Input: Y.Y.. as an example
+            char[] temp = schedule.toCharArray();
+            for (int j = 0; j < temp.length; j++) {
+                if (temp[j] == 'Y') {
+                    days[j] += 1;
+                }
+            }
+        }
+        // Finding the optimal day
+        int best_day = 0;
+        int attending = 0;
+        for (int day = 0; day < days.length; day++) {
+            int current_attending = days[day];
+            if (current_attending > attending) {
+                best_day = day;
+                attending = current_attending;
+            }
+        }
+        // Check for ties
+        int counter = 0;
+        for (int day = 0; day < days.length; day++) {
+            int current_attending = days[day];
+            if (current_attending == attending) {
+                System.out.println("Day" + (day+1) + " is an optimal day.");
+                counter += 1
+            }
+        }
+        sc.close();
+    }
+}
